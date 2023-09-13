891b3e99760069cfee3e7daa5117f24a77e3f223
RCSilva1964 <robertocostaesilva@hotmail.com>
2023-09-13 20:19:00 -0300

    Cap. 5 (Field of View) concluido.

--- src/components.rs
+++ src/components.rs
@@ -1,6 +1,6 @@
 use specs::prelude::*;
 use specs_derive::*;
-use rltk::{RGB};
+use rltk::RGB;
 
 #[derive(Component)]
 pub struct Position {
@@ -18,3 +18,9 @@ pub struct Renderable {
 #[derive(Component, Debug)]
 pub struct Player {}
 
+#[derive(Component)]
+pub struct Viewshed {
+    pub visible_tiles : Vec<rltk::Point>,
+    pub range : i32,
+    pub dirty : bool
+}
