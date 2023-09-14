c9a0e2cb3a83d93dcdb8cdf464b26e610f043281
RCSilva1964 <robertocostaesilva@hotmail.com>
2023-09-14 12:58:57 -0300

    Cap. 6 (Monsters) concluido.
    Este é meu primeiro "commit" usando o lazygit.

--- src/components.rs
+++ src/components.rs
@@ -1,6 +1,6 @@
+use rltk::RGB;
 use specs::prelude::*;
 use specs_derive::*;
-use rltk::RGB;
 
 #[derive(Component)]
 pub struct Position {
@@ -16,11 +16,19 @@ pub struct Renderable {
 }
 
 #[derive(Component, Debug)]
+pub struct Name {
+    pub name: String,
+}
+
+#[derive(Component, Debug)]
 pub struct Player {}
+
+#[derive(Component, Debug)]
+pub struct Monster {}
 
 #[derive(Component)]
 pub struct Viewshed {
-    pub visible_tiles : Vec<rltk::Point>,
-    pub range : i32,
-    pub dirty : bool
+    pub visible_tiles: Vec<rltk::Point>,
+    pub range: i32,
+    pub dirty: bool,
 }
